da0de723d44454824a35344cce7cf6c2051b72c7
kirvy810 <99127833+kirvy810@users.noreply.github.com>
2022-03-30 22:50:08 +0900

    Update primality-test.py

--- Python/primality-test.py
+++ Python/primality-test.py
@@ -1,13 +1,10 @@
-from math import sqrt
-
-
 def is_prime(n):
     if n < 2:
         return False
     if n % 2 == 0:
         return n == 2
 
-    for i in range(3, int(sqrt(n)) + 1, 2):
+    for i in range(3, int(n ** 0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
