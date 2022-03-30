eaef20a5018d2cc44cce45ea292f2b79ad185976
kirvy810 <99127833+kirvy810@users.noreply.github.com>
2022-03-30 22:57:23 +0900

    Update sieve.py

--- Python/sieve.py
+++ Python/sieve.py
@@ -6,16 +6,13 @@ def main():
     sieve[0] = False
     sieve[1] = False
 
-    for i in range(2, int(N ** 0.5) + 1):
+    for i in range(2, N + 1):
         if not sieve[i]:
             continue
 
+        print(i, end=' ')
         for j in range(2 * i, N + 1, i):
             sieve[j] = False
-
-    for n, is_prime in enumerate(sieve):
-        if is_prime:
-            print(n, end=' ')
 
 
 if __name__ == '__main__':
