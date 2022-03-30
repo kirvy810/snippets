f5ffdbd49395be6380bb98c8bb3d6fd69fef855e
kirvy810 <99127833+kirvy810@users.noreply.github.com>
2022-03-30 22:59:50 +0900

    Update sieve.py

--- Python/sieve.py
+++ Python/sieve.py
@@ -1,17 +1,15 @@
 def main():
-    N = 100
+    N = 100 + 1
 
-    sieve = [True] * (N + 1)
-
+    sieve = [True] * N
     sieve[0] = sieve[1] = False
 
-    for i in range(2, N + 1):
-        if not sieve[i]:
-            continue
+    for i in range(2, N):
+        if not sieve[i]: continue
 
         print(i, end=' ')
         
-        for j in range(2 * i, N + 1, i):
+        for j in range(2 * i, N, i):
             sieve[j] = False
 
 
