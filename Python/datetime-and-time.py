34c46431fc85a3edbd76d7fdbf4d8fa8faed40d8
kirvy810 <99127833+kirvy810@users.noreply.github.com>
2022-05-18 11:26:19 +0900

    Update datetime-and-time.py

--- Python/datetime-and-time.py
+++ Python/datetime-and-time.py
@@ -16,6 +16,10 @@ def get_day(tm):
     return '월화수목금토일'[tm.tm_wday]
 
 
+def us_date_format(tm):
+    return '{}-{}-{}'.format(tm.tm_mday, get_month(tm), tm.tm_year)
+
+
 def main():
     now = time.localtime()
     current_date = datetime.date(now.tm_year, now.tm_mon, now.tm_mday)
@@ -23,12 +27,12 @@ def main():
     y, m, d = map(int, input('YYYY/MM/DD > ').split('/'))
     date = datetime.date(y, m, d)
 
-    print('오늘은 {}-{}-{}'.format(now.tm_mday, get_month(now), now.tm_year))
+    print('오늘은 {}입니다.'.format(us_date_format(now)))
 
     diff = diff_days(current_date, date)
 
     if diff < 0:
-        print('{}부터 {}일이 지났습니다'.format(-date, diff))
+        print('{}부터 {}일이 지났습니다'.format(date, -diff))
     elif diff > 0:
         print('{}까지 {}일이 남았습니다'.format(date, diff))
     else:
